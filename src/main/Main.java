d5dddf2532b1a519e664cd9c4490eaa571cb845c
fandbsoft <fandbsoft@gmail.com>
2026-09-17 12:30:47 +0700

    ok

--- src/main/Main.java
+++ src/main/Main.java
@@ -39,7 +39,8 @@ public class Main {
 			testDevice(label, ip, port, pwd);
 		} else {
 			System.out.println("Chay kiem thu mac dinh tren cac thiet bi LAN co san:\n");
-			testDevice("MAY: ZKTECO iSCAN-03 / ZEM560 (LEGACY)", "192.168.1.190", ZKTeco4370_ZkConstants.DEFAULT_PORT, 111111);
+//			testDevice("MAY: ZKTECO iSCAN-03 / ZEM560 (LEGACY)", "192.168.1.190", ZKTeco4370_ZkConstants.DEFAULT_PORT, 111111);
+			testDevice("MAY: ZKTECO iSCAN-03 / ZEM560 (LEGACY)", "192.168.1.33", ZKTeco4370_ZkConstants.DEFAULT_PORT, 111111);
 //			testDevice("MAY: ZKTECO iSCAN-03 / ZEM560 (LEGACY)", "42.112.179.197", ZKTeco4370_ZkConstants.DEFAULT_PORT, 111111);
 		}
 
